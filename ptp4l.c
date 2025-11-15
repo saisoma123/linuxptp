887fd152cde838e3cd7bf1cdcbe097a6652d6c6a
Surya Somayyajula <suryasai.somayyajula@gmail.com>
2025-11-15 12:37:52 +0000

    Closest working incremental attack in millisecond range

--- ptp4l.c
+++ ptp4l.c
@@ -317,8 +317,8 @@ int main(int argc, char *argv[])
         memset(&tx, 0, sizeof(tx));
 
         tx.modes = ADJ_FREQUENCY;
-        double bias = 1.1;
-        tx.freq = (long) (50 * 65.536);
+        double bias = 1.05;
+        tx.freq = (long) (125 * 65.536);
 
         int sync_sock = -1;
 
