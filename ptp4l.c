a5060c5249c070e0476fb3f645473d56575c6e2b
Surya Somayyajula <suryasai.somayyajula@gmail.com>
2025-11-17 07:00:38 +0000

    Constant policy runs

--- ptp4l.c
+++ ptp4l.c
@@ -50,6 +50,8 @@
 #define CLOCKID_TO_FD(clk)  ((unsigned int) ~((clk) >> 3))
 #define FD_TO_CLOCKID(fd) ((~(clockid_t) (fd) << 3) | CLOCKFD)
 
+
+static const int64_t ERROR = 483302;
 int sync_sock = -1;
 static int open_sync_socket(const char *iface)
 {
@@ -191,12 +193,14 @@ static void timeguard_policy_c_step(struct clock *c)
     bool trusted = tg_watchdog_error(phc_ns, &err_out);
     // pr_notice("master: sec=%ld  secure=%ld\n",(long)get_master_offset(c), (long)err_out.nanoseconds);
     // pr_notice("trusted: %s\n", trusted ? "true" : "false");
-	if (!trusted) {
-    /* --- Combine TimeGuard error --- */
     int64_t err_ns =
-        (int64_t)err_out.seconds * 1000000000LL +
-        (int64_t)err_out.nanoseconds;
+            (int64_t)err_out.seconds * 1000000000LL +
+            (int64_t)err_out.nanoseconds;
 
+    if (err_ns > ERROR || err_ns < -ERROR) {
+        /* --- Combine TimeGuard error --- */
+        err_ns -= ERROR;
+    
     int64_t master_ns = get_master_offset(c);  /* in ns, from ptp servo */
 
     /* --- Digit scaling --- */
@@ -284,11 +288,13 @@ static void timeguard_policy_a_step(struct clock *c)
     // pr_notice("master: sec=%ld  secure=%ld\n",(long)get_master_offset(c), (long)err_out.nanoseconds);
     // pr_notice("trusted: %s\n", trusted ? "true" : "false");
 
-    if (!trusted) {
-        /* --- Combine TimeGuard error --- */
-        int64_t err_ns =
+    int64_t err_ns =
             (int64_t)err_out.seconds * 1000000000LL +
             (int64_t)err_out.nanoseconds;
+
+    if (err_ns > ERROR || err_ns < -ERROR) {
+        /* --- Combine TimeGuard error --- */
+        err_ns -= ERROR;
 
         int64_t master_ns = get_master_offset(c);  /* in ns, from ptp servo */
 
@@ -388,13 +394,13 @@ static void timeguard_policy_b_step(struct clock *c)
     bool trusted = tg_watchdog_error(phc_ns, &err_out);
     // pr_notice("master: sec=%ld  secure=%ld\n",(long)get_master_offset(c), (long)err_out.nanoseconds);
     // pr_notice("trusted: %s\n", trusted ? "true" : "false");
-
-    if (!trusted) {
-        /* --- Combine TimeGuard error --- */
-        int64_t err_ns =
+    int64_t err_ns =
             (int64_t)err_out.seconds * 1000000000LL +
             (int64_t)err_out.nanoseconds;
 
+    if (err_ns > ERROR || err_ns < -ERROR) {
+        /* --- Combine TimeGuard error --- */
+        err_ns -= ERROR;
         int64_t master_ns = get_master_offset(c);  /* in ns, from ptp servo */
 
         /* --- Digit scaling --- */
